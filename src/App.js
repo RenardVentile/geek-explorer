0dc85cf026d7fcfdf99e03f2e98d6c7a11e594ad
Valentin <derrevalentin@gmail.com>
2019-08-07 15:53:00 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import SideNav, {
+  NavItem,
+  NavIcon,
+  NavText
+} from "@trendmicro/react-sidenav";
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
+import UnixPage from './components/UnixPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route
+          render={({ location, history }) => (
+            <React.Fragment>
+              <SideNav
+                style={{ background: 'black' }}
+                onSelect={selected => {
+                  const to = "/" + selected;
+                  if (location.pathname !== to) {
+                    history.push(to);
+                  }
+                }}
+              >
+                <SideNav.Toggle />
+                <SideNav.Nav defaultSelected="home">
+                  <NavItem eventKey="">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-home"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Home</NavText>
+                  </NavItem>
+                  <NavItem eventKey="page1">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-pencil-ruler"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Graphisme</NavText>
+                  </NavItem>
+                  <NavItem eventKey="page2">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-font"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Typographie</NavText>
+                  </NavItem>
+                  <NavItem eventKey="page2">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-pencil-alt"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Illustration</NavText>
+                  </NavItem>
+                  <NavItem eventKey="page2">
+                    <NavIcon>
+                      <i
+                        className="fa fa-fw fa-camera"
+                        style={{ fontSize: "1.75em" }}
+                      />
+                    </NavIcon>
+                    <NavText>Photographie</NavText>
+                  </NavItem>
+                </SideNav.Nav>
+              </SideNav>
+              <main>
+                <Route path="/" exact component={UnixPage} />
+              </main>
+            </React.Fragment>
+          )}
+        />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/UnixPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class UnixPage extends Component {
+  render() {
+    return (
+      <div>
+        coucou unix
+      </div>
+    )
+  }
+}
+
+export default UnixPage;
