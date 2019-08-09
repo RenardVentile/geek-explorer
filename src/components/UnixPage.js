eaea4258162cc4c7309098ee81ef0ad5cd472cd8
Spessotto <eva@spessotto.com>
2019-08-09 19:26:01 +0200

    Add functions for connecting api to front

--- src/components/UnixPage.js
+++ src/components/UnixPage.js
@@ -1,6 +1,44 @@
 import React, { Component } from "react";
 import { Container } from "reactstrap";
 
+// onClick(firstOption) {
+// 	if (firstOption.hasSecondOption === true) {
+// 		displaySecondSelect(event.target.id);
+// 	}
+// 	showResponse(event.target.id);
+// }
+
+// displaySecondSelect(id) {
+// 	axios
+// 		.get(`http://api/secondOption/${id}`)
+// 		.then(res => res.data)
+// 		.then(data => {
+// 			this.setState({ response: data });
+// 		})
+// 		.catch(err => this.setState({ error: err.message }));
+
+// 	return (
+// 		<FormGroup>
+// 			<Input type="select">
+// 				{options.map(option => {
+// 					return <option>{option}</option>;
+// 				})}
+// 			</Input>
+// 		</FormGroup>
+// 	);
+// }
+
+// showResponse(id) {
+// 	axios
+// 		.get(`http://api/usage/${id}`)
+// 		.then(res => res.data)
+// 		.then(data => {
+// 			this.setState({ response: data });
+// 		})
+// 		.catch(err => this.setState({ error: err.message }));
+
+// 	return <div>{this.state.response}</div>;
+// }
 class UnixPage extends Component {
   render() {
     return <Container>coucou unix</Container>;
